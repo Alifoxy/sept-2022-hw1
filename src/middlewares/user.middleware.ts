72f150c7d3103ac2393deededf2db5fb5231cc6c
Alifoxy <nightfoxy666@gmail.com>
2023-03-11 17:14:55 +0300

    new

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { isObjectIdOrHexString } from "mongoose";
 
 import { ApiError } from "../errors/api.error";
+import { IRequest } from "../types/common.types";
 import { User } from "../models/User.model";
 import { UserValidator } from "../validators";
 
@@ -25,6 +26,55 @@ class UserMiddleware {
     } catch (e) {
       next(e);
     }
+  }
+
+  public getDynamicallyAndThrow(
+      fieldName: string,
+      from = "body",
+      dbField = fieldName
+  ) {
+    return async (req: IRequest, res: Response, next: NextFunction) => {
+      try {
+        const fieldValue = req[from][fieldName];
+
+        const user = await User.findOne({ [dbField]: fieldValue });
+
+        if (user) {
+          throw new ApiError(
+              `User with ${fieldName} ${fieldValue} already exist`,
+              409
+          );
+        }
+
+        next();
+      } catch (e) {
+        next(e);
+      }
+    };
+  }
+
+  public getDynamicallyOrThrow(
+      fieldName: string,
+      from = "body",
+      dbField = fieldName
+  ) {
+    return async (req: IRequest, res: Response, next: NextFunction) => {
+      try {
+        const fieldValue = req[from][fieldName];
+
+        const user = await User.findOne({ [dbField]: fieldValue });
+
+        if (!user) {
+          throw new ApiError(`User not found`, 422);
+        }
+
+        req.res.locals = user;
+
+        next();
+      } catch (e) {
+        next(e);
+      }
+    };
   }
 
   public async isUserIdValid(
